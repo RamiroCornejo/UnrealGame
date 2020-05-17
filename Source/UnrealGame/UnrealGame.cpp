9de203c417f77e745b5a2f3c07556391fcf55c4c
RamiroCornejo <ramiro.cornejo@davinci.edu.ar>
2020-05-16 22:54:56 -0300

    Initial commit

---
+++ Source/UnrealGame/UnrealGame.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealGame, "UnrealGame" );
+ 
